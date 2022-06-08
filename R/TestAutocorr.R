c49aaa5c0bbf4ea5fe2872d391a0a4b5e90962c7
PierreRlld <p.rouillard@outlook.com>
2022-06-08 17:35:47 +0200

    update nomenclature

--- R/TestAutocorr.R
+++ R/TestAutocorr.R
@@ -3,17 +3,17 @@
 #' @param var série stationnaire (d=0); type timeSeries
 #' @param p ordre partie autorégressive à tester
 #' @param q ordre partie moyenne mobile à tester
-#' @param lag_max lag max à tester pour les autocorrélations des résidus (statistique de Ljung-Box) > hypothèse forte résidus suivent un bruit blanc, test jusqu'à lag_max
+#' @param lagmax lag max à tester pour les autocorrélations des résidus (statistique de Ljung-Box) > hypothèse forte résidus suivent un bruit blanc, test jusqu'à lag_max
 #'
 #' @return lag=i et p-value du test de Portemanteau (test d'indépendance des résidus -> white noise ? ; le test vérifie sur un nb de lag donné si les autocorrélations entre résidus sont nuls) avec statistique de Ljung-Box. p-value <0.05 on rejette l'hypothèse nulle au seuil de 95% i.e. rejette l'hypothèse d'indépendance des résidus.
 #' @export
 
-TestAutocorr<-function(var,p,q,lag_max){
+TestAutocorr<-function(var,p,q,lagmax){
   autocorL<-list()
   fitdf<-p+q
   serie<-arima(var,order=c(p,0,q),include.mean=FALSE)
   rej<-0
-  for (i in c(1:lag_max)){
+  for (i in c(1:lagmax)){
     if (i<=fitdf) {NA}
     else {
       cat("lag=",i,"pvalue=",Box.test(residuals(serie),lag=i,type="Ljung-Box",fitdf = fitdf)$p.value,"\n")  #type="Ljung-Box",
--- R/armafit.R
+++ R/armafit.R
@@ -3,16 +3,16 @@
 #' @param serie série stationnaire d'entrée
 #' @param p ordre partie autorégressive à tester
 #' @param q ordre partie moyenne mobile à tester
-#' @param lag_max lag max à tester pour les autocorrélations des résidus (statistique de Ljung-Box) > hypothèse forte résidus suivent un bruit blanc, test jusqu'à lag_max
+#' @param lagmax lag max à tester pour les autocorrélations des résidus (statistique de Ljung-Box) > hypothèse forte résidus suivent un bruit blanc, test jusqu'à lag_max
 #'
 #' @return Synthese de TestAutocorr et TestSignificativite
 #' @export
 #'
-armafit<-function(serie,p,q,lag_max){
+armafit<-function(serie,p,q,lagmax){
   cat("Try ARMA(",p,",",q,")\n")
   cat("\n")
   cat("1/ Test d'absence absence d'autocorrelation des residus [Ljung-Box] :\n")
-  TestAutocorr(serie,p,q,lag_max)
+  TestAutocorr(serie,p,q,lagmax)
   cat(" \n")
   cat("2/ Test de nullite des coefficients des des ordres les plus eleves :\n")
   TestSignificatif(serie,p,q)
